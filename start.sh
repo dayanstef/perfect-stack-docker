d8316fdc5abb1af711424869a124845ddc57d60d
D <d.stefanoski@picturae.com>
2019-02-15 14:53:12 +0100

    updates start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sed -i "s|sendfile on|sendfile off|" /etc/nginx/nginx.conf
+sed -i "s|;extension=php_pdo_pgsql.dll|extension=php_pdo_pgsql.dll|" /etc/php/7.2/fpm/php.ini
+sed -i "s|;extension=php_pgsql.dll|extension=php_pgsql.dll|" /etc/php/7.2/fpm/php.ini
+
 # ONLY IF IS LARAVEL APPLICATION
 if [[ "${LARAVEL_APP}" == "1" ]]; then
     # RUN LARAVEL MIGRATIONS ON BUILD.
@@ -8,9 +12,3 @@ if [[ "${LARAVEL_APP}" == "1" ]]; then
         php artisan migrate
     fi
 fi
-
-sed -i "s|sendfile on|sendfile off|" /etc/nginx/nginx.conf
-sed -i "/tty/!s/mesg n/tty -s \\&\\& mesg n/" /root/.profile
-sed -i "s|;cgi.fix_pathinfo=1|cgi.fix_pathinfo=0|" /etc/php/7.2/cli/php.ini
-sed -i "s|;extension=php_pdo_pgsql.dll|extension=php_pdo_pgsql.dll|" /etc/php/7.2/fpm/php.ini
-sed -i "s|;extension=php_pgsql.dll|extension=php_pgsql.dll|" /etc/php/7.2/fpm/php.ini
