23435b76beedd0136263c065fefc613d4b3cd740
D <d.stefanoski@picturae.com>
2019-02-19 09:41:58 +0100

    entry point restarts services

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-cd ${APP_ROOT}
-
-# ONLY IF IS LARAVEL APPLICATION
-if [[ "${LARAVEL_APP}" == "1" ]]; then
-    make install-backend
-    make install-frontend
-    make npm-run-prod
-
-    # RUN LARAVEL MIGRATIONS ON BUILD.
-    if [[ "${RUN_LARAVEL_MIGRATIONS_ON_BUILD}" == "1" ]]; then
-        php artisan migrate
-    fi
-fi
+service redis-server restart
+service memcached restart
+service postgresql restart
+service nginx restart
